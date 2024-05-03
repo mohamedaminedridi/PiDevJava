e91b7c826879cc95cd3e169076f1ac439adad43a
mohamedaminedridi <mohamedamine.dridi@esprit.tn>
2024-05-03 14:51:58 +0100

    PiDev

--- src/main/java/main/FXMain.java
+++ src/main/java/main/FXMain.java
@@ -20,7 +20,7 @@ public class FXMain extends Application {
 
             Connection cnx;
             cnx = MyDatabase.getInstance().getConnection();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/VoyageListeFront.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/voyageList.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             primaryStage.setTitle("sami");
